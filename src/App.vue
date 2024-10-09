6b6703f2a64cc868f00e078abbc62817eb27b7bd
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-10-09 12:16:46 +0200

    init repo

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AlertComponent from "./components/AlertComponent.vue";
+
+</script>
+
+<template>
+  <div class="container">
+    <AlertComponent msg="Vite + Vue" />
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
