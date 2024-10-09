59ddfaddc27eeef361e3225982b3733a05ff4322
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-10-09 12:41:25 +0200

    excompleted

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <script setup>
 import AlertComponent from "./components/AlertComponent.vue";
 
+function hadleEvent() {
+  console.log("Hai chiuso un alert");
+}
 </script>
 
 <template>
   <div class="container">
-    <AlertComponent msg="Vite + Vue" />
+    <AlertComponent type="info"> Informazioni Ottenute </AlertComponent>
+    <AlertComponent type="danger"> Informazioni Pericolose </AlertComponent>
+    <AlertComponent type="warning"> NON PUOI FRAMI QUESTO</AlertComponent>
+    <AlertComponent type="success" @closed="hadleEvent">
+      Puoi fare cio' che vuoi
+    </AlertComponent>
   </div>
-
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/AlertComponent.vue
+++ src/components/AlertComponent.vue
@@ -1,17 +1,33 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps({
-  msg: String,
-})
+const props = defineProps({
+  type: String,
+});
 
-const count = ref(0)
+const emit = defineEmits(["closed"]);
+
+const tag = ref();
+tag.value = props.type;
+
+const count = ref(0);
+
+function functionClicked() {
+  emit("closed");
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="alert alert-dismissible fade show" :class="`alert-${props.type}`">
+    <strong><slot></slot></strong>
+    <button
+      @click="functionClicked()"
+      type="button"
+      class="btn-close"
+      data-bs-dismiss="alert"
+      aria-label="Close"
+    ></button>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
